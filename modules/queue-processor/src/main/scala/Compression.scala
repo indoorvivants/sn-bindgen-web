3a48d046e3f97149ad4f3886da8be894327d18e1
Anton Sviridov <keynmol@gmail.com>
2023-05-27 12:43:22 +0100

    Add decompression

--- modules/queue-processor/src/main/scala/Compression.scala
+++ modules/queue-processor/src/main/scala/Compression.scala
@@ -6,10 +6,12 @@ import cats.effect.*
 import scala.scalanative.unsafe.*
 import scala.scalanative.unsigned.*
 import scala.scalanative.runtime.libc
+import scodec.bits.ByteVector
+import zstd.all.*
 
 object Compression:
+  // adapted from https://github.com/facebook/zstd/blob/dev/examples/simple_compression.c
   def compressString(code: String): Array[Byte] = Zone { implicit z =>
-    import zstd.all.*
     val inBuf      = toCString(code)
     val inBufSize  = size_t(code.getBytes().length.toULong)
     val outBufSize = ZSTD_compressBound(inBufSize)
@@ -25,5 +27,21 @@ object Compression:
     libc.memcpy(bytes.at(0), outBuf, resultSize.value)
 
     bytes
+  }
+
+  // Adapted from https://github.com/facebook/zstd/blob/dev/examples/simple_decompression.c#L38
+  def decompressByteVector(bv: ByteVector): Array[Byte] = Zone { implicit z =>
+    val arr   = bv.toArray
+    val cSize = size_t(arr.length.toULong)
+    val cBuff = arr.at(0)
+    val rSize = ZSTD_getFrameContentSize(cBuff, cSize)
+    val rBuff = alloc[Byte](rSize)
+    val dSize = ZSTD_decompress(rBuff, size_t(rSize), cBuff, cSize)
+
+    val bytes = new Array[Byte](rSize.toInt)
+    libc.memcpy(bytes.at(0), rBuff, rSize)
+
+    bytes
+
   }
 end Compression
